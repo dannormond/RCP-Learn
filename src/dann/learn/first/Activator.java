200577683bdfb3399ff50c7b60f2751b67c0ae48
dann <dann@i7-work>
2010-07-28 22:12:19 -0600

    Changed all de.vogella references to dann.learn

--- src/dann/learn/first/Activator.java
+++ src/dann/learn/first/Activator.java
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "de.vogella.rcp.intro.first"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "dann.learn.first"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
